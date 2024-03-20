91df41fe43c6c873e19dd86d62810a5b136bd976
Noah <noahjobse@gmail.com>
2024-03-20 16:00:32 -0600

    Changed HTML for site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    # Your view logic here
+    return render(request, 'blog/post_list.html', {})
+
 
 # Create your views here.
